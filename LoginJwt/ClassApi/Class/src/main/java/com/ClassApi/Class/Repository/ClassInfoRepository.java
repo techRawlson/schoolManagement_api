759bfede6b148d2b435dcb74178a8d237e969c11
aman aggarwal <amana9383@gmail.com>
2024-04-29 21:56:17 -0700

    StaffSerivce-API

--- LoginJwt/ClassApi/Class/src/main/java/com/ClassApi/Class/Repository/ClassInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ClassApi.Class.Repository;
-
-import com.ClassApi.Class.Entities.ClassInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ClassInfoRepository extends JpaRepository<ClassInfo, Long> {
-}
